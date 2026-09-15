b9938301daf8265bc66d2c072b092e8334ea0ef6
Henrique <henriqueagria08@gmail.com>
2026-09-15 12:59:25 -0300

    fiz ex1, 2 e 3

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -4,8 +4,15 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
+	
+#include <climits>
+	cout << "int  max = " << INT_MAX << "\n";
+	cout << "int  min = " << INT_MIN << "\n";
+	cout << "short max = " << SHRT_MAX << "\n";
+
 	cout << "Estrutura de Dados - Fatec (2026/2)" << endl;
 
 	
@@ -59,17 +66,39 @@ int main()
 	int dias = 0;
 	int horas = 0;
 	int minutos = 0;
+	double graus = 0;
+	int fahrenheit = 0;
+	int kelvin = 0;
+	long segundos = 0;
+	int dias2 = 0;
+	int horas2 = 0;
+	int minutos2 = 0;
 
 	cout << "Digite seu nome: ";
 	cin >> nome;
 	cout << "Digite sua idade: ";
 	cin >> idade;
+	cout << "Digite quanto graus celsius está hoje: ";
+	cin >> graus;
+	cout << "Digite um número de segundos: ";
+	cin >> segundos;
 	dias = idade * 365;
 	horas = dias * 24;
 	minutos = horas  * 60;
+	fahrenheit = graus * 1.8 + 32;
+	kelvin = graus + 273,15;
+	dias2 = segundos / 86400;
+	horas2 = segundos / 3600;
+	minutos2 = segundos / 60;
 	cout << "Sua idade em dias e: " << dias << endl;
 	cout << "Sua idade em horas e: " << horas << endl;
 	cout << "Sua idade em minutos e: " << minutos << endl;
+	cout << "A conversão de graus celsius para fahrenheit é: " << fahrenheit << endl;
+	cout << "A conversão de graus celsius para kelvin é: " << kelvin << endl;
+	cout << "A conversão de segundos para dias é: " << dias2 << endl;
+	cout << "A conversão de segundos para horas é: " << horas2 << endl;
+	cout << "A conversão de segundos para minutos é: " << minutos2 << endl;
+	
 
 	cout << "Qualquer tecla para sair...";
 	system("pause"); // somente no Windows
